1b95f081156416a2fe1809cd572a82b27c1dad94
CarterChilds <carterchilds@live.com>
2018-06-14 17:02:00 -0600

    first commit

---
+++ src/components/Bots/Bot.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <div>
+          <button @click='onFight'>FIGHT</button>
+      </div>
+      <div v-for='bot in battleBots' :key='bot.id'>
+        <p>Selected: {{bot.name, bot.name}}</p>
+        <hr>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+props: ['battleBots'],
+
+methods: {
+    onFight(){
+        var winner = this.battleBots[Math.floor(Math.random()*this.battleBots.length)];
+       alert(winner.name + ' ' + "is the winner!")
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Bots/BotList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      <div>
+      <bot :battleBots = 'battleBots'></bot>
+      </div>
+      <div v-for='(bot) in bots' :key='bot.id' >
+         <p>Bot Name: {{bot.name}}</p>
+         <p>Health: {{bot.health}}</p></span>
+          <p>Attack: {{bot.attack}}</p></span>
+          <button @click='addToBattle(bot.id)'>Select</button>
+         <hr>
+      </div>
+  </div>
+</template>
+
+<script>
+import Bot from './Bot'
+export default {
+  props: ["bots"],
+
+  components: {
+      bot: Bot
+  },
+
+  data() {
+    return {
+      battleBots: []
+    };
+  },
+
+  methods: {
+   addToBattle(id) {
+     for (let i = 0; i < this.bots.length; i++) {
+       if (id === this.bots[i].id) {
+         this.battleBots.push(this.bots[i]);
+         console.log('added', this.battleBots)
+       }
+     }
+   }
+  }
+};
+</script>
+
+<style>
+
+</style>
